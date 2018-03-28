5945aa8577e95356571f2935ef1a26bb5d40a680
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-03-28 08:51:01 +0200

    Added get last modified and access time of a file, instant's output

--- com/sm/net/util/FileFolder.java
+++ com/sm/net/util/FileFolder.java
@@ -1,13 +1,18 @@
 package com.sm.net.util;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 
  * @author SM-Net <http://sm-netzwerk.com>
  * 
  *         Utility Class for Files and Folders
- *
  */
 public class FileFolder {
 
@@ -18,7 +23,7 @@ public class FileFolder {
 	 * @param directory
 	 * @return list files and folders or null
 	 */
-	public static File[] listAllFilesAndFolders(File directory) {
+	public static File[] listAllFiles(File directory) {
 
 		if (directory != null) {
 			if (directory.exists()) {
@@ -26,6 +31,73 @@ public class FileFolder {
 					return directory.listFiles();
 				}
 			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * Returns the instant (last modified time) of the file
+	 * 
+	 * @param file
+	 * @return instant or null
+	 * @throws IOException
+	 */
+	public static Instant getFileLastModifiedInstant(File file) {
+
+		if (file != null) {
+			if (file.exists()) {
+				if (file.isFile()) {
+					try {
+						return Files.getLastModifiedTime(file.toPath()).toInstant();
+					} catch (IOException e) {
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * Returns the instant (last access time) of the file
+	 * 
+	 * @param file
+	 * @return instant or null
+	 * @throws IOException
+	 */
+	public static Instant getFileLastAccessInstant(File file) {
+
+		if (file != null) {
+			if (file.exists()) {
+				if (file.isFile()) {
+					BasicFileAttributes attributes = null;
+					try {
+						attributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+						return attributes.lastAccessTime().toInstant();
+					} catch (IOException e) {
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * Returns the string for the instant's output
+	 * 
+	 * @param instant
+	 * @param format
+	 *            For example: dd.MM.yyyy HH:mm:ss
+	 * @return string or null
+	 */
+	public static String printInstant(Instant instant, String format) {
+
+		if (instant != null) {
+			DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format).withZone(ZoneId.systemDefault());
+
+			return dateTimeFormatter.format(instant);
 		}
 		return null;
 	}
