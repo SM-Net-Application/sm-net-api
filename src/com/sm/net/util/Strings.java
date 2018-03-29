8b98d77950f1b10e8f11f98391fe0864c670440c
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-03-29 09:23:40 +0200

    Added new utils

--- src/com/sm/net/util/Strings.java
+++ src/com/sm/net/util/Strings.java
@@ -1,5 +1,6 @@
 package com.sm.net.util;
 
+import java.io.UnsupportedEncodingException;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -21,12 +22,54 @@ public class Strings {
 	 *            For example: dd.MM.yyyy HH:mm:ss
 	 * @return string or null
 	 */
-	public static String printInstant(Instant instant, String format) {
+	public static String instantToString(Instant instant, String format) {
 
 		if (instant != null) {
 			DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format).withZone(ZoneId.systemDefault());
 			return dateTimeFormatter.format(instant);
 		}
 		return null;
+	}
+
+	/**
+	 * 
+	 * Returns the string for the millisecond's output
+	 * 
+	 * @param millisecond
+	 * @param format
+	 *            For example: dd.MM.yyyy HH:mm:ss
+	 * @return string or null
+	 */
+	public static String millisecondToString(long millisecond, String format) {
+
+		Instant instant = Instant.ofEpochMilli(millisecond);
+
+		if (instant != null)
+			return instantToString(instant, format);
+
+		return null;
+	}
+
+	/**
+	 * 
+	 * Returns the string recoded with charset
+	 * 
+	 * @param string
+	 * @param charset
+	 *            For example: UTF-8
+	 * @return the same string or the recoded string
+	 */
+	public static String recoding(String string, String charset) {
+
+		String recodedString = string;
+
+		byte[] bytes = string.getBytes();
+
+		try {
+			recodedString = new String(bytes, charset);
+		} catch (UnsupportedEncodingException e) {
+		}
+
+		return recodedString;
 	}
 }
