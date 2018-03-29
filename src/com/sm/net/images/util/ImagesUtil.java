270fd1e731bd69df88bf1d5c2d5b4e1fe0070604
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-03-29 11:15:46 +0200

    Utility Class for Image Files

---
+++ src/com/sm/net/images/util/ImagesUtil.java
@@ -0,0 +1,81 @@
+package com.sm.net.images.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+
+/**
+ * 
+ * @author SM-Net <http://sm-netzwerk.com>
+ * 
+ *         Utility Class for Image Files
+ */
+public class ImagesUtil {
+
+	/**
+	 * Returns the object Metadata of a image file
+	 * 
+	 * @param file
+	 * @return Metadata object or null
+	 */
+	public static Metadata getImageMetadata(File file) {
+
+		try {
+			return ImageMetadataReader.readMetadata(file);
+		} catch (ImageProcessingException | IOException e) {
+		}
+
+		return null;
+	}
+
+	/**
+	 * Returns the Camera Model metadata
+	 * 
+	 * @param metadata
+	 * @return
+	 */
+	public static String getCameraModel(Metadata metadata) {
+
+		String cameraModel = "";
+
+		ExifIFD0Directory exifIFD0Directory = metadata
+				.getFirstDirectoryOfType(com.drew.metadata.exif.ExifIFD0Directory.class);
+
+		if (exifIFD0Directory != null) {
+
+			cameraModel = exifIFD0Directory.getDescription(272);
+			if (cameraModel.length() > 0) {
+				return cameraModel.trim();
+			}
+		}
+		return cameraModel;
+	}
+
+	/**
+	 * Returns the DateTimeOriginal
+	 * 
+	 * @param metadata
+	 * @return
+	 */
+	public static String getDateTimeOriginal(Metadata metadata) {
+
+		String dateTimeOriginal = "";
+
+		ExifSubIFDDirectory exifSubIFDDirectory = metadata
+				.getFirstDirectoryOfType(com.drew.metadata.exif.ExifSubIFDDirectory.class);
+
+		if (exifSubIFDDirectory != null) {
+			dateTimeOriginal = exifSubIFDDirectory.getDescription(36867);
+			if (dateTimeOriginal.length() > 0) {
+				return dateTimeOriginal.trim();
+			}
+		}
+
+		return dateTimeOriginal;
+	}
+}
